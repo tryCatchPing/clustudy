9ec92bffe43bcf5f836633f27f551c3638afc2e3
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2025-08-07 03:49:37 +0900

    design: 노트 화면 figma 디자인 변환 테스트 ## 변환 완전 성공적 - 디렉토리 확정 - feature: 로직만 제공, 추후 위젯 삭제 - design: 모든 디자인 시작
    ai 변환 페이지 제공으로 각 요소들 뜯어와서 조정하고 수정하고 적용하면 될 듯
    
    ## 디자인 demo 페이지 제작
    - 라우터로 이동 가능
    - 각 요소별 테스트 가능
    - 추후 widgetbook 사용도 가능하나 시간 부족으로 현 방향 유지 예정

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'features/canvas/routing/canvas_routes.dart';
 import 'features/home/routing/home_routes.dart';
 import 'features/notes/routing/notes_routes.dart';
+import 'design_system/routing/design_system_routes.dart';
 
 void main() => runApp(const ProviderScope(child: MyApp()));
 
@@ -16,6 +17,8 @@ final _router = GoRouter(
     ...NotesRoutes.routes,
     // 캔버스 관련 라우트 (노트 편집)
     ...CanvasRoutes.routes,
+    // 디자인 시스템 데모 라우트 (컴포넌트 쇼케이스, Figma 재현)
+    ...DesignSystemRoutes.routes,
   ],
   debugLogDiagnostics: true,
 );
