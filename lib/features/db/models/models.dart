55027a7eaad0c844f80ca8e3199ff864226a9e46
jidamkim <zidam715@naver.com>
2025-08-21 03:50:30 +0900

    feat: models.dart 배럴 파일에 새로운 features 모델들 추가
    - NoteModel, NotePageModel, PdfCacheMetaModel export 추가
    - 기존 vault_models.dart와 함께 새로운 모델들 노출
    - 단일 진입점을 통한 모델 접근 가능
    - 점진적 마이그레이션 지원

--- lib/features/db/models/models.dart
+++ lib/features/db/models/models.dart
@@ -1,4 +1,9 @@
 // Barrel file for DB models
 export 'vault_models.dart';
 
+// Features models with Isar annotations
+export 'package:it_contest/features/notes/models/note_model.dart';
+export 'package:it_contest/features/notes/models/note_page_model.dart';
+export 'package:it_contest/features/pdf_cache/models/pdf_cache_meta_model.dart';
+
 
