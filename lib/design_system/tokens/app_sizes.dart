803cd1f209279cb69d78a8188007c2a37e5aaea6
yul-04 <yurakim0829@gmail.com>
2025-09-10 00:42:59 +0900

    폴더용 그리드 생성

--- lib/design_system/tokens/app_sizes.dart
+++ lib/design_system/tokens/app_sizes.dart
@@ -6,5 +6,10 @@ class AppSizes {
   static const double noteTileW  = 120;
   static const double noteTileH  = 152;
 
+  static const double folderIconW = 144;
+  static const double folderIconH = 136;
+  static const double folderTileW = 144;
+  static const double folderTileH = 196;
+
   static const double addIcon    = 32; // '+' 아이콘
 }
