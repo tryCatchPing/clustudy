5c406d3a835ee449cd806fae1a825a2391bf2b8d
yul-04 <yurakim0829@gmail.com>
2025-09-10 00:53:11 +0900

    폴더 카드 icon 넣기

---
+++ lib/design_system/tokens/app_icons.dart
@@ -0,0 +1,7 @@
+// lib/design_system/tokens/app_icons.dart
+class AppIcons {
+  AppIcons._();
+  static const folder      = 'assets/icons/folder.svg';
+  static const folderVault = 'assets/icons/folder_vault.svg';
+  static const plus        = 'assets/icons/plus.svg';
+}
