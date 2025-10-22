ec8ad15412c798f2d23f77ff48c9eeebddd36e2b
ehdnd_mac <taeungjang@naver.com>
2025-10-22 20:33:15 +0900

    fix: 패키지명 변경 com.clustudy.clustudy
    TODO:
    - base64 인코딩 후 secrete에 넣어야해요

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
     // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
@@ -21,7 +22,7 @@ val hasReleaseKeystore = if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.trycatchping.clustudy"
+    namespace = "com.clustudy.clustudy"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.13599879"
 
@@ -35,7 +36,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.trycatchping.clustudy"
+        applicationId = "com.clustudy.clustudy"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
